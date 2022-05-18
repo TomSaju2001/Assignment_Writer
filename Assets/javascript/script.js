109877374a542928851a08af772c10b49a33cf74
TOM SAJU <tomsaju33@gmail.com>
2022-05-18 15:41:34 +0530

    Test 1

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,1 +1,16 @@
-
+window.onload = function () {
+	var fileupload = document.getElementById('pageUploader');
+	var button = document.getElementById('btnFileUpload');
+	button.onclick = function () {
+		fileupload.click();
+	};
+	fileupload.onchange = function () {
+		var reader = new FileReader();
+		reader.readAsDataURL(fileupload.files[0]);
+		reader.onload = function (event) {
+			img = loadImage(event.target.result, () => {
+				loop();
+			});
+		};
+	};
+};
